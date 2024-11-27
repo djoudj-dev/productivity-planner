75c3f5d1f4f9e73957f6fb80403adfb40514bded
djoudj-dev <nedellec.julien.78@gmail.com>
2024-11-27 19:34:30 +0100

    Refactor app component: replace staging info with production details and update environment variable usage for clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'productivity-planner';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly isStaging = !(environment as any).production;
+  isProductionEnvironment = environment.production;
+  firebaseProjectId = environment.firebaseConfig.projectId;
 }

91e9be7325cd7fde59ae2d5fae0dfe5a329236dd
djoudj-dev <nedellec.julien.78@gmail.com>
2024-12-12 09:42:25 +0100

    feat(auth): add register user to server firebase

---
+++ src/app/core/authentication.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.dev';
+
+/**
+ * Represents the payload of the response received when registering a new user in Firebase.
+ *
+ * @see https://firebase.google.com/docs/reference/rest/auth?hl=fr#section-create-email-password
+ */
+interface FirebaseResponseRegister {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthenticationService {
+  readonly #http = inject(HttpClient);
+
+  register(
+    email: string,
+    password: string
+  ): Observable<FirebaseResponseRegister> {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`;
+    const body = {
+      email,
+      password,
+      returnSecureToken: true,
+    };
+    return this.#http.post<FirebaseResponseRegister>(url, body);
+  }
+}

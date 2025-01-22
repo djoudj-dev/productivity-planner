06e32da305b8c72f540925af93365e8c276ff9a4
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-22 16:47:16 +0100

    chore(lint) : configure ESLint et Stylelint avec intégration de Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
+const prettier = require("eslint-plugin-prettier");
 
 module.exports = tseslint.config(
   {
@@ -11,9 +12,33 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      "plugin:prettier/recommended",
     ],
     processor: angular.processInlineTemplates,
+    parserOptions: {
+      project: ["tsconfig.json"],
+      createDefaultProgram: true,
+    },
+    globals: {
+      sessionStorage: true,
+      localStorage: true,
+      navigator: true,
+      window: true,
+      setTimeout: true,
+      clearTimeout: true,
+      setInterval: true,
+      clearInterval: true,
+      btoa: true,
+      atob: true,
+      BigInt: true,
+    },
     rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-console": ["error", { allow: ["warn", "error"] }],
+      "linebreak-style": ["error", "unix"],
+      "@typescript-eslint/no-non-null-assertion": "error",
       "@angular-eslint/directive-selector": [
         "error",
         {
@@ -30,14 +55,50 @@ module.exports = tseslint.config(
           style: "kebab-case",
         },
       ],
+      "prefer-template": "warn",
+      "no-eval": "error",
+      curly: "error",
+    },
+  },
+  {
+    files: ["**/*.spec.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+      project: ["tsconfig.spec.json"],
     },
   },
   {
     files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
+    extends: [...angular.configs.templateRecommended],
+    rules: {
+      "@angular-eslint/template/attributes-order": [
+        "error",
+        {
+          alphabetical: false,
+          order: [
+            "STRUCTURAL_DIRECTIVE",
+            "TEMPLATE_REFERENCE",
+            "ATTRIBUTE_BINDING",
+            "INPUT_BINDING",
+            "TWO_WAY_BINDING",
+            "OUTPUT_BINDING",
+          ],
+        },
+      ],
+      "@angular-eslint/template/prefer-self-closing-tags": ["error"],
+      "@angular-eslint/template/prefer-control-flow": ["error"],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    excludeFiles: ["*inline-template-*.component.html"],
+    extends: ["plugin:prettier/recommended"],
+    rules: {
+      "prettier/prettier": ["error", { parser: "angular" }],
+    },
   }
 );

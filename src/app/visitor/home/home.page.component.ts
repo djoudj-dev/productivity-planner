4136b950f8f22c883a08958deb7a4da5c6482945
djoudj-dev <nedellec.julien.78@gmail.com>
2025-01-24 10:49:30 +0100

    feat(home): ajout de l'image de la bannière d'accueil et intégration du composant de liste des cartes de fonctionnalités

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HomeBannerDumbComponent } from './home-banner/home-banner.dumb.component';
+import { HomeFeatureCardListDumbComponent } from './home-feature-card-list/home-feature-card-list.dumb.component';
 
 @Component({
   standalone: true,
-  imports: [HomeBannerDumbComponent],
+  imports: [HomeBannerDumbComponent, HomeFeatureCardListDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss',
 })
 export class HomePageComponent {
+  featureCardList = signal([
+    {
+      name: 'Planifier sa semaine',
+      icon: 'calendar',
+      description: 'Visibilité sur les 7 prochains jours',
+    },
+    {
+      name: 'Atteindre ses objectifs',
+      icon: 'target',
+      description: 'Suivi de ses objectifs',
+    },
+    {
+      name: 'Analyser sa productivité',
+      icon: 'chart-line',
+      description: 'Suivi de votre productivité et de vos objectif',
+    },
+  ]);
+
   // onBannerClicked
   onBannerClicked() {
     console.log('Banner clicked');

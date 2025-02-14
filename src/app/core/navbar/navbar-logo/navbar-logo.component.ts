20054c2c9d0c296b342963d93d786697eff1ced7
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-14 18:16:59 +0100

    feat(component:ui): Ajoute le composant NavbarLogo
    Ajoute un nouveau composant `NavbarLogoComponent` pour intégrer le logo dans la barre de navigation. Comprend l'utilisation d'un fichier SVG pour le template.

---
+++ src/app/core/navbar/navbar-logo/navbar-logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-logo',
+  imports: [],
+  templateUrl: './navbar-logo.component.svg',
+})
+export class NavbarLogoComponent {}

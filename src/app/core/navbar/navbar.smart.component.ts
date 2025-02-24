caad8423ec23a4841bbc9f955637f48bda14afb8
djoudj-dev <nedellec.julien.78@gmail.com>
2025-02-24 14:45:30 +0100

    feat(navbar): Add RouterLink to signup page and update component tests
    - Update navbar component to use RouterLink for signup navigation
    - Add appConfig providers to component and app tests
    - Import RouterLink in navbar component

--- src/app/core/navbar/navbar.smart.component.ts
+++ src/app/core/navbar/navbar.smart.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavbarLogoComponent } from './navbar-logo/navbar-logo.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NavbarLogoComponent],
+  imports: [NavbarLogoComponent, RouterLink],
   templateUrl: './navbar.smart.component.html',
   styleUrl: './navbar.smart.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,

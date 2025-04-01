772a3d3ee7f278760e94ce56e38af809c1809984
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-01 22:47:47 +0200

    fix(service:api): Corrige l'accès aux clés Firebase dans l'environnement
    - Remplace `firebaseConfig` par `firebase` pour corriger les références aux clés dans les services d'auth et user.
    - Garantit le bon fonctionnement des requêtes API utilisant les configurations Firebase.

--- src/app/core/adapter/user-firebase.service.ts
+++ src/app/core/adapter/user-firebase.service.ts
@@ -8,10 +8,9 @@ import { User } from '../entity/user.interface';
 @Injectable()
 export class UserFirebaseService implements UserService {
   readonly #http = inject(HttpClient);
-  readonly #FIRESTORE_URL =
-    'https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents';
+  readonly #FIRESTORE_URL = `https://firestore.googleapis.com/v1/projects/${environment.firebase.projectId}/databases/(default)/documents`;
   readonly #USER_COLLECTION_ID = 'users';
-  readonly #FIREBASE_API_KEY = environment.firebaseConfig.apiKey;
+  readonly #FIREBASE_API_KEY = environment.firebase.apiKey;
   readonly #USER_COLLECTION_URL = `${this.#FIRESTORE_URL}/${this.#USER_COLLECTION_ID}?key=${this.#FIREBASE_API_KEY}&documentId=`;
 
   create(user: User, bearerToken: string): Observable<void> {

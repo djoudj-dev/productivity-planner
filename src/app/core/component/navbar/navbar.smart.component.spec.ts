cd40139be2d80cac233738a73053f798efbe8f48
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-01 15:57:34 +0200

    refactor(component:navbar): Réorganise la structure des fichiers
    - Déplace les fichiers du composant Navbar dans un nouveau dossier pour une meilleure organisation.
    - Ajuste les imports nécessaires suite à ce déplacement.

--- src/app/core/component/navbar/navbar.smart.component.spec.ts
+++ src/app/core/component/navbar/navbar.smart.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthenticationService } from '../authentication.service';
+import { AuthenticationService } from '../../port/authentication.service';
 import { NavbarSmartComponent } from './navbar.smart.component';
 
 describe('NavbarSmartComponent', () => {

d8e3efeaa2438623461f7e581d0ee9a10a147e91
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-01 21:50:49 +0200

    test(component:navbar): Remplace RouterTestingModule par provideRouter
    - Met à jour les tests en remplaçant RouterTestingModule par provideRouter.
    - Simplifie la configuration des tests en utilisant une approche plus adaptée.

--- src/app/core/component/navbar/navbar.smart.component.spec.ts
+++ src/app/core/component/navbar/navbar.smart.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AuthenticationService } from '../../port/authentication.service';
 import { NavbarSmartComponent } from './navbar.smart.component';
 
@@ -9,8 +9,9 @@ describe('NavbarSmartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarSmartComponent, RouterTestingModule],
+      imports: [NavbarSmartComponent],
       providers: [
+        provideRouter([]),
         {
           provide: AuthenticationService,
           useValue: {

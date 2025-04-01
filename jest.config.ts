64059ec26571f630c22219a1858339f35794b02b
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-01 22:23:13 +0200

    chore(test): Supprime les tests obsolètes et le mapping d'environnement mock
    - Retire des fichiers de tests unitaires inutilisés et leur configuration associée.
    - Élimine le moduleNameMapper pour les environnements mock dans la configuration Jest.

--- jest.config.ts
+++ jest.config.ts
@@ -92,8 +92,6 @@ const config: Config = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@environments/(.*)$': '<rootDir>/src/environments/$1',
-    '^.*environments/environment$':
-      '<rootDir>/src/environments/environment.mock.ts',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader

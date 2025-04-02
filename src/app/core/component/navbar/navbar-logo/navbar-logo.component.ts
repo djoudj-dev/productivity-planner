70ee52602e1cf7a9ff70577e189b8a0138318fab
djoudj-dev <nedellec.julien.78@gmail.com>
2025-04-02 15:55:06 +0200

    refactor(component:navbar): Supprime les styles inutilisés du logo
    - Retire le fichier SCSS non utilisé dans `NavbarLogoComponent`.
    - Met à jour le fichier HTML pour ajouter un `routerLink` au logo.
    - Corrige les configurations Angular liées au serveur de développement et à l'extraction i18n.

--- src/app/core/component/navbar/navbar-logo/navbar-logo.component.ts
+++ src/app/core/component/navbar/navbar-logo/navbar-logo.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar-logo.component.svg',
-  styleUrl: './navbar-logo.component.scss',
 })
 export class NavbarLogoComponent {}

38915bb99fd5a881ef63ea8fe583cc35005b9331
djoudj-dev <nedellec.julien.78@gmail.com>
2025-06-11 10:16:20 +0200

    feat: Ajoute la configuration Jest et les fichiers d'environnement pour le développement et la mise en scène fix(login): Améliore la gestion des erreurs et la soumission du formulaire de connexion refactor(user-firebase): Met à jour les URL de la collection utilisateur pour Firestore chore: Ajoute des fichiers de configuration pour les tests et met à jour les scripts de test

--- src/app/core/adapter/user-firebase.service.ts
+++ src/app/core/adapter/user-firebase.service.ts
@@ -1,9 +1,9 @@
-import { UserService } from '../port/user.service';
-import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { environment } from '@environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { map, Observable } from 'rxjs';
 import { User } from '../entity/user.interface';
+import { UserService } from '../port/user.service';
 
 @Injectable()
 export class UserFirebaseService implements UserService {
@@ -11,10 +11,9 @@ export class UserFirebaseService implements UserService {
   readonly #FIRESTORE_URL = `https://firestore.googleapis.com/v1/projects/${environment.firebaseConfig.projectId}/databases/(default)/documents`;
   readonly #USER_COLLECTION_ID = 'users';
   readonly #FIREBASE_API_KEY = environment.firebaseConfig.apiKey;
-  readonly #USER_COLLECTION_URL = `${this.#FIRESTORE_URL}/${this.#USER_COLLECTION_ID}?key=${this.#FIREBASE_API_KEY}&documentId=`;
 
   create(user: User, bearerToken: string): Observable<void> {
-    const url = `${this.#USER_COLLECTION_URL}${user.id}`;
+    const url = `${this.#FIRESTORE_URL}/${this.#USER_COLLECTION_ID}/${user.id}?key=${this.#FIREBASE_API_KEY}`;
     const body = {
       fields: {
         name: { stringValue: user.name },
@@ -31,7 +30,7 @@ export class UserFirebaseService implements UserService {
   }
 
   fetch(userId: string, bearerToken: string): Observable<User> {
-    const url = `${this.#USER_COLLECTION_URL}${userId}`;
+    const url = `${this.#FIRESTORE_URL}/${this.#USER_COLLECTION_ID}/${userId}?key=${this.#FIREBASE_API_KEY}`;
     const headers = new HttpHeaders({
       Authorization: `Bearer ${bearerToken}`,
     });
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAHehfydKfUVxq66SElh24mI8vRGB4Yks0',
+    authDomain: 'productivity-planner-dev-52e7f.firebaseapp.com',
+    projectId: 'productivity-planner-dev-52e7f',
+    storageBucket: 'productivity-planner-dev-52e7f.firebasestorage.app',
+    messagingSenderId: '432598389859',
+    appId: '1:432598389859:web:4a672ee000b19517189456',
+  },
+};

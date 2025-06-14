5b14ec07afece471753e18e84a75322baa2beb52
djoudj-dev <nedellec.julien.78@gmail.com>
2025-06-14 11:47:51 +0200

    feat(workday): Ajoute le composant Workday avec la gestion des tâches et l'état associé

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { WorkDayStore } from './workday.page.store';
 
 @Component({
   imports: [],
   templateUrl: './workday.page.component.html',
   styleUrl: './workday.page.component.scss',
+  providers: [WorkDayStore],
 })
-export class WorkdayPageComponent {}
+export class WorkdayPageComponent {
+  readonly store = inject(WorkDayStore);
+}
